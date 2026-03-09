fe3964791fa146ec34a5a9a847b865724f77d12d
Juan David Hernandez Altamiranda <juandavidhernandezaltamiranda@gmail.com>
2026-03-09 11:42:59 -0500

    terminacion del body, faltan las animaciones

---
+++ script.js
@@ -0,0 +1,19 @@
+// scroll para los enlaces
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) { 
+          const offset = 80;
+          const elementPosition = targetElement.getBoundingClientRect().top;
+          const offsetPosition = elementPosition + window.pageYOffset - offset;
+          
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
